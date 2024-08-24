fd23a764e65151b828af9883e711fced044bd634
chayapatr <chayapatr.arc@gmail.com>
2024-08-24 22:19:24 +0700

    feat: params

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
 	const shuffle = (arr: string[]) => arr.sort(() => Math.random() - 0.5);
+	import { page } from '$app/stores';
+
+	const tParam = $page.url.searchParams.get('t');
 
 	let i = 0;
+
 	let t: 'og' | 'ai' = 'ai';
+	if (tParam && ['og', 'ai'].includes(tParam)) {
+		t = tParam as 'og' | 'ai';
+	}
 
 	const tps: string[] = ['dl', 'dc', 'ns'];
 	const og = shuffle(
@@ -33,13 +40,13 @@
 <div class="flex h-[100vh] w-[100vw] flex-col items-center justify-center gap-10">
 	<div class="flex items-center gap-8 text-4xl">
 		<button class="btn" on:click={() => (i > 0 ? i-- : i)}>←</button>
-		<div class="aspect-square w-[50vw] max-w-md rounded-md bg-red-500">
+		<div class="aspect-square w-[40vw] max-w-md rounded-md bg-red-500">
 			<img src={`imgs/${imgs[t][i]}`} class="h-full w-full object-cover" alt="" />
 		</div>
 		<button class="btn" on:click={() => (i < imgs[t].length - 1 ? i++ : i)}>→</button>
 	</div>
 
-	<div class="text-4xl">{i + 1} of {imgs[t].length}</div>
+	<div class="text-3xl">{i + 1} of {imgs[t].length}</div>
 
 	<!-- <button
 		class="rounded-md bg-black p-2 text-lg text-white hover:bg-neutral-900"
