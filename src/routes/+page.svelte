4905d23f7b1cd8620d8e37ecf1408b7d95714914
chayapatr <chayapatr.arc@gmail.com>
2024-08-24 20:28:11 +0700

    feat: imgs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	const imgs: number[] = [1, 2, 3, 4, 5];
-	let i: number = 1;
+	const shuffle = (arr: string[]) => arr.sort(() => Math.random() - 0.5);
+
+	let i = 0;
+	const tps: string[] = ['dl', 'dc', 'ns'];
+	const imgs = shuffle(
+		tps.map((tp) => [...Array.from({ length: 8 }, (_, i) => `${tp}${i + 1}.jpg`)]).flat()
+	);
+
+	console.log(imgs);
 </script>
 
 <div class="flex h-[100vh] w-[100vw] flex-col items-center justify-center gap-10">
 	<div class="flex items-center gap-8 text-4xl">
-		<button class="btn" on:click={() => (i > 1 ? i-- : i)}>←</button>
-		<div class="aspect-square w-[50vw] max-w-md rounded-md bg-red-500">{i}</div>
-		<button class="btn" on:click={() => (i < imgs.length ? i++ : i)}>→</button>
+		<button class="btn" on:click={() => (i > 0 ? i-- : i)}>←</button>
+		<div class="aspect-square w-[50vw] max-w-md rounded-md bg-red-500">
+			<img src={`imgs/og/${imgs[i]}`} class="h-full w-full object-cover" alt="" />
+		</div>
+		<button class="btn" on:click={() => (i < imgs.length - 1 ? i++ : i)}>→</button>
 	</div>
 
-	<div class="text-4xl">{i} of {imgs.length}</div>
+	<div class="text-4xl">{i + 1} of {imgs.length}</div>
 </div>
 
 <style>
